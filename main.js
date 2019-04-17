f8b3e25930b9b953b7087518d118f64524aca2d1
Blake <2979337714@qq.com>
2019-04-17 21:08:05 +0800

    封装ajax

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 button.addEventListener('click',()=>{
     let request = new XMLHttpRequest()
     request.open('post','/blake')
@@ -15,3 +16,32 @@ button.addEventListener('click',()=>{
     }
     request.send()
 })
+*/
+button.addEventListener('click',()=>{
+    window.jQurey.ajax({
+        url:'/blake',
+        method:'post',
+        body:'hello'
+    }).then(
+        (text)=>{console.log(text)},
+        (request)=>{console.log(request)}
+    )
+})
+
+window.jQurey = function (){}
+window.jQurey.ajax = function ({url,method,body}){
+    return new Promise(function (resolve,reject){
+        let request = new XMLHttpRequest()
+        request.open(method,url)
+        request.onreadystatechange = ()=>{
+            if(request.readyState === 4){
+                if(request.status >= 200 && request.status < 300){
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status >= 400){
+                    reject.call(undefined, request)
+                }
+            }
+        }
+        request.send(body)
+    })
+}
